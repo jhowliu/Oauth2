ecc5741ff49c0877429d3f1c6934f49a7e2e3962
Jhow <jhowliu@gmail.com>
2018-05-31 16:55:23 +0800

    add callback url

--- settings/index.js
+++ settings/index.js
@@ -2,5 +2,5 @@ export default {
   app_id: `APP_ID`,
   app_secret: `APP_SECRET`,
   app_token: `APP_ACCESS_TOKEN`,
-  redirect_uri: `REDIRECT_URI`,
+  redirect_uri: `http://localhost:8999/v1/facebook/callback`,
 }
