0cf741e9dcf18a0f232d57798fa3f082679b9d7d
jhow <jhowliu@gmail.com>
2018-06-07 01:00:36 +0800

    - instagram Oauth2 Works and remove axios because it cannot post with form-data

--- route/facebook.js
+++ route/facebook.js
@@ -1,45 +1,63 @@
-import Axios from 'axios';
 import Express from 'express';
 import Settings from '../settings';
+import Querystring from 'querystring';
+
+import { access } from 'fs';
+import { makeRequest } from '../lib/httpUtils';
 
 const router = Express.Router()
 
 router.get('/connect', (req, res) => {
   const app_id = Settings.facebook.app_id;
-  const redirect_uri = Settings.facebook.redirect_uri;
-  const oauth_url = `https://www.facebook.com/v3.0/dialog/oauth?client_id=${app_id}&redirect_uri=${redirect_uri}&scope=manage_pages`
+  const params = Querystring.stringify({
+    client_id: app_id,
+    scope: `manage_pages`,
+    redirect_uri: Settings.facebook.redirect_uri
+  });
+  const oauth_url = `https://www.facebook.com/v3.0/dialog/oauth?${params}`;
 
   res.json({ redirect_uri : oauth_url });
 });
 
 router.get('/callback', (req, res) => {
-  const access_token_url = `https://graph.facebook.com/v3.0/oauth/access_token`
+  const access_token_url = `https://graph.facebook.com/v3.0/oauth/access_token`;
 
-  Axios
-  .get(access_token_url, {
+  makeRequest(access_token_url, `GET`, {
     params: {
+      code: req.query.code,
       client_id: Settings.facebook.app_id,
       client_secret: Settings.facebook.app_secret,
-      code: req.query.code,
       redirect_uri: Settings.facebook.redirect_uri
     }
   })
-  .then(response => response.data)
-  .then(data => {
-    const access_token = data.access_token;
+  .then(response => response.json())
+  .then(json => {
+
+    if (json.error) {
+      return res.json({
+        success: false,
+        errors: json.error.message
+      })
+    }
+
     const inception_token_url = `https://graph.facebook.com/debug_token`;
-    Axios.get(inception_token_url, {
+    makeRequest(inception_token_url, `GET`, {
       params: {
-        input_token: access_token,
-        access_token: Settings.app_token
+        input_token: json.access_token,
+        access_token: Settings.facebook.app_token
       }
     })
-    .then(response => response.data)
-    .then(data => res.json(data));
+    .then(response => response.json())
+    .then(json => res.json(json))
+    .catch(err => res.json({
+      success: false,
+      errors: err.toString()
+    }));
   })
-  .catch(err => {
-    res.json({ success: false, error: `${err.response.data}` });
-  })
+  .catch(err => res.json({
+    success: false,
+    errors: err.toString()
+  }));
 });
 
 export default router;
--- route/instagram.js
+++ route/instagram.js
@@ -1,6 +1,8 @@
-import Axios from 'axios';
 import Express from 'express';
 import Settings from '../settings';
+
+import { access } from 'fs';
+import { makeRequest, makeFormData } from '../lib/httpUtils';
 
 const router = Express.Router()
 
@@ -8,25 +10,37 @@ router.get('/connect', (req, res) => {
   const app_id = Settings.instagram.app_id;
   const redirect_url = Settings.instagram.redirect_uri;
   const oauth_url = `https://api.instagram.com/oauth/authorize/?client_id=${app_id}&redirect_uri=${redirect_url}&response_type=code`
+
   res.json({'redirect_url': oauth_url});
 });
 
 router.get('/callback', (req, res) => {
   const access_token_url = `https://api.instagram.com/oauth/access_token`
   const code = req.query.code;
-  console.log(code)
-  Axios.post(access_token_url, {
-    grand_type: `authorization_code`,
-    client_id: Settings.instagram.app_id,
-    client_secret: Settings.instagram.app_secret,
-    redirect_uri: Settings.instagram.redirect_uri,
-    code
+
+  const formData = makeFormData({
+    code, 
+    'grant_type': `authorization_code`,
+    'client_id': Settings.instagram.app_id,
+    'client_secret': Settings.instagram.app_secret,
+    'redirect_uri': Settings.instagram.redirect_uri
+  });
+
+  makeRequest(access_token_url, `POST`, {
+    body: formData
   })
-  .then(response => {
-    console.log(response)
+  .then(response => response.json())
+  .then(json => {
+    res.json({
+      success: true,
+      message: json
+    })
   })
   .catch(err => {
-    console.log(err.toString())
+    res.json({
+      success: false,
+      errors: err.toString()
+    })
   });
 });
 
